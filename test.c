77a321fe7fa11e4e54d20b5f6703a100a31743c1
Andrey Mazo <ahippo@yandex.com>
2014-03-19 00:27:18 +0400

    move atoi() out of timed loop.
    resurrected double to avoid precision loss in loops/msec calculation.

--- test.c
+++ test.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /* a - b in msecs */
-static long timespec_sub(const struct timespec *a, const struct timespec *b) {
+static double timespec_sub(const struct timespec *a, const struct timespec *b) {
 	long secs = a->tv_sec - b->tv_sec;
 	long nanosecs = a->tv_nsec - b->tv_nsec;
 	if (nanosecs < 0) {
@@ -11,7 +11,7 @@ static long timespec_sub(const struct timespec *a, const struct timespec *b) {
 		nanosecs += 1000000000L;
 	}
 
-	return secs * 1000 + nanosecs / 1000000;
+	return secs * 1000 + nanosecs / 1000000.;
 }
 
 
@@ -21,13 +21,13 @@ int main(int argc, char *argv[]) {
 	}
 	double f1 = atof(argv[1]);
 	double f2 = atof(argv[2]);
+	long iterations = atoi(argv[3]) * 1000 * 1000;
 	double ans = 1.0;
 	struct timespec start;
 	int rc = clock_gettime(CLOCK_MONOTONIC, &start);
 	if (rc < 0) {
 		exit(1);
 	}
-	long iterations = atoi(argv[3]) * 1000 * 1000;
 	for (long i = 0; i < iterations; i++) {
 		ans *= f1;
 		ans /= f2;
